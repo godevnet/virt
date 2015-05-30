af694866dc65722ab8ffdc73f158a865d68ca1e3
root <goffinet@goffinet.eu>
2015-05-30 17:57:20 +0200

    revision

--- autovm.sh
+++ autovm.sh
@@ -1,57 +1,115 @@
 #/bin/bash
 
 ## autovm.sh file
-## usage : autovm.sh [baseline] "[domain list]"
-##         autovm.sh $1 "$2" 
+## usage : autovm.sh [baseline] [method] "[domain list]"
+##         autovm.sh $1 $2 "$3"
+## ex :    autovm.sh small http "vm00 vm01 vm02 vm03" 
 ## Linux KVM domain automatic deployment Fedora/Centos 7
-## baseline : small, medium ou large
-## domain list between " "
+## via HTTP repo the options are :
+##  * baseline : small, medium ou large
+##  * method : http or local to get Kickstart
+##  * domain list between " "
 ##
 
-## Variables
+## Internal variables 
 baseline=$1
-list=$2
-## $uidtemp var : template uid
+method=$2
+list=$3
+# count domain in $list
+nb=$(echo $list | wc -w)
+# $uidtemp var : template uid
 uidtemp="gi-$(uuidgen | cut -d - -f 1)"
-## bridge network
+
+## Options to configure (network and paths)
+## Base scenario with libvirt Default network
+## and local http browser for ks and installation files
+# bridge network
 bridge=virbr0
 bridgeip4=$(ip -4 address show $bridge | grep 'inet' | sed 's/.*inet \([0-9\.]\+\).*/\1/')
-## $vol var : path to domain images
+# $vol var : path to domain images
 vol=/var/lib/libvirt/images
-## $www var : local path to template kickstart file
+# $www var : local path to template kickstart file
 www=/var/www/html/conf
-## $conf var : http path to template kickstart file 
+# $conf var : http path to template kickstart file 
 conf=http://$bridgeip4/conf
-## $mirror var : http path to an installation mirrot
+# $mirror var : http path to an installation mirrot
 mirror=http://$bridgeip4/repo
 ## Public mirrors
 #mirror=http://centos.mirrors.ovh.net/ftp.centos.org/7/os/x86_64
 #mirror=http://ftp.belnet.be/ftp.centos.org/7/os/x86_64
 #mirror=http://mirror.i3d.net/pub/centos/7/os/x86_64
-## count domain in $list
-nb=$(echo $list | wc -w)
+
+## Driving the script ?
+## 1. Check the five options variables 
+## 2. Launch the script with the baseline machine, the kickstart file location 
+## and the name of your new domains between double quotes as arguments.
+## 3. Look to your console and please read the logs. 
+## If the newly created domains already exist, the script ask you what to do with them. 
+## Assume that you have a local http server when you an http kickstart installation
+##
+## What the script is doing for you ?
+## 1. Checking the configuration 
+## 2. Generation of a fresh temporary template via http repo and local or remote ks file
+## 3. Building clones from the template domain
+## 4. Preparing, optimizing, customizing each new virtual disk
+## 5. Starting and monitoring new domains
+##
+## Automation goal ?
+## This script is used to deploy a numberous of idendial domains 
+## on a libvirtd/libguestfs host. 
+## 
+## What are the next goals ?
+## But the "use case" is followed by some next steps.
+## After this operation, the goal is to manage those new linux para-virtualized virtual machines
+## via Ansible and deploy configurations and applications (native or container based as Docker)
+##
+## Source of this script : practice lab in Linux training classroom with a motived team
 
 install_info ()
 {
 
 # Show install configuation 
-echo "* Install configuration : $baseline"
-echo "* $nb domains to create : $list"
-echo "* Path to images : $vol"
-echo "* FS path to kickstart file : $www"
-echo "* http path to kickstart file : $conf"
-echo "* Mirroir des fichiers d'installation : $mirror"
+echo "+ Install configuration : $baseline"
+echo "+ Kickstart file method : $method"
+echo "+ $nb domains to create : $list"
+echo "+ Path to images : $vol"
+echo "+ FS path to kickstart file : $www"
+echo "+ http path to kickstart file : $conf"
+echo "+ Mirroir des fichiers d'installation : $mirror"
 
 }
 
-temp_erase ()
+dom_erase ()
 {
 
+# Template already present ? Probably not
 echo "Erasing template"
 virsh destroy $uidtemp 2> /dev/null
 virsh undefine $uidtemp 2> /dev/null
 rm -f $vol/$uidtemp.*
 rm -f $www/$uidtemp.*
+
+# Domains to deploy already present ?
+for ((i=1;i<=$nb;i++)); do
+domain=$(echo $list | cut -d" " -f $i)
+if $(virsh list --all | grep -w "$domain" &> /dev/null); then
+        read -p "$domain domain  exists. Erasing (y/n) ? " answer
+        if [ $answer = 'y' ]
+         then
+                /bin/virsh destroy $domain 2> /dev/null
+                /bin/virsh undefine $domain 2> /dev/null
+                rm -rf $vol/$domain.* 2> /dev/null
+        elif [ $answer = 'n' ]
+         then
+                list=$(echo "$list" | sed "s/$domain//g")
+                i=$((i-1))
+                nb=$((nb-1))
+        else
+                i=$((i-1))
+                continue
+        fi
+fi
+done
 
 }
 
@@ -62,22 +120,26 @@ ks_prep ()
 {
 
 ## Kickstart file preparation
-echo "Kickstart file preparation : @core+SSH key+LV 4G"
+echo "Kickstart file preparation"
 
-##
+# Put packages line by line between EOM as variable $packages
+read -r -d '' packages <<- EOM
+@core
+wget
+EOM
+
 touch $www/$uidtemp.ks
 cat << EOF > $www/$uidtemp.ks 
 install
-keyboard --vckeymap=be-oss --xlayouts='be (oss)'
 reboot
 rootpw --plaintext testtest
+keyboard --vckeymap=be-oss --xlayouts='be (oss)'
 timezone Europe/Brussels
-url --url="$mirror"
 lang fr_BE
+url --url="$mirror"
 firewall --disabled
 network --bootproto=dhcp --device=eth0
 network --hostname=$uidtemp
-# !!!
 # network --device=eth0 --bootproto=static --ip=192.168.22.10 --netmask 255.255.255.0 --gateway $bridgeip4 --nameserver=$bridgeip4 --ipv6 auto
 auth  --useshadow  --passalgo=sha512
 text
@@ -93,7 +155,7 @@ part pv.00 --fstype="lvmpv" --ondisk=vda --size=500 --grow
 volgroup local0 --pesize=4096 pv.00
 logvol /  --fstype="xfs"  --size=4000 --name=root --vgname=local0
 %packages
-@core
+$packages
 %end
 %post
 #yum -y update
@@ -104,6 +166,7 @@ sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_
 EOF
 
 chown apache:apache $www/$uidtemp.ks
+
 }
 
 virt_install ()
@@ -112,10 +175,11 @@ virt_install ()
 installation ()
 {
 
-## Template domain installation start ... 
-echo "Template \"$baseline\" domain installation is starting  ... "
+playtemp ()
+{
 ## virt-install process installation via http following kickstart file config
-nohup \
+echo "Template \"$baseline\" domain installation is starting  ... "
+#nohup \
 /bin/virt-install \
 --virt-type kvm \
 --name=$uidtemp \
@@ -128,28 +192,34 @@ nohup \
 --noreboot \
 --console pty,target_type=serial \
 --location $mirror \
--x "ks=$conf/$uidtemp.ks console=ttyS0,115200n8 serial" \
-> /dev/null 2>&1 &
-
-## cd-rom installation and local kickstart
-#ks=/var/www/html/conf
-#iso=path/to/iso
-#--cdrom $iso \
-#--initrd-inject=/$uidtemp.ks -x "ks=file:/$uidtemp.ks console=ttyS0,115200n8 serial" \
-
+$init \
+-x " $extra "  
+#> /dev/null 2>&1 &
 sleep 5
-
 while (true) do
         check_install=$(virsh list | grep $uidtemp 2> /dev/null)
 	echo -n "."
 	sleep 3
-
-if [ -z "$check_install" ]; then
-break
-fi
+	if [ -z "$check_install" ]; then
+		break
+	fi
 done
 
 echo -e "\nTemplate is created ! "
+}
+
+if [ $method = http ] ; then
+	extra="ks=$conf/$uidtemp.ks console=ttyS0,115200n8 serial"
+	init=""
+	playtemp
+elif [ $method = local ] ; then
+        extra="console=ttyS0,115200n8 serial"
+	init="--initrd-inject=/$uidtemp.ks"
+	playtemp
+else
+        exit
+fi
+
 
 }
 
@@ -177,7 +247,7 @@ fi
 
 }
 
-temp_erase
+dom_erase
 ks_prep
 virt_install
 
@@ -185,40 +255,6 @@ virt_install
 
 clone ()
 {
-
-dom_man ()
-{
-
-# Domains to deploy already present ?
-
-for ((i=1;i<=$nb;i++)); do
-
-domain=$(echo $list | cut -d" " -f $i)
-
-if $(virsh list --all | grep -w "$domain" &> /dev/null); then
-
-        read -p "$domain domain  exists. Erasing (y/n) ? " answer
-        if [ $answer = 'y' ]
-         then
-                /bin/virsh destroy $domain 2> /dev/null
-		/bin/virsh undefine $domain 2> /dev/null
-		rm -rf $vol/$domain.* 2> /dev/null
-        elif [ $answer = 'n' ]
-         then
-		list=$(echo "$list" | sed "s/$domain//g")
-                i=$((i-1))
-                nb=$((nb-1))
-        else
-                i=$((i-1))
-                continue
-        fi
-
-fi
-
-done
-
-}
-
 sysprep ()
 {
 ## !!!
@@ -251,7 +287,6 @@ done
 
 }
 
-dom_man
 sysprep
 cloning
 }
